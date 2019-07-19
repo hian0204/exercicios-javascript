61c30a8c0636b4cef9b5c928c55b65e1f45caf37
hian0204 <hiansilva0204@gmail.co4>
2019-07-19 17:54:07 -0300

    atividade de javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+let idade =prompt("digite sua idade");
+
+let salario = prompt("digite o valor do seu do seu salario");
+
+
+let emprestimo = prompt("digite o valor do seu emprestimo");
+
+console.log(`Olá eu tenho ${idade}, meu salario é ${idade}, e solicito ${emprestimo}`);
+
+
+if(idade >= 25 && idade <= 60){
+    console.log(`aprovado`);
+    
+}
+
+else()
+
+
+
+
